26b1c9e7984190c8294929f6f569826ffe6c2b05
multa-notitia <115737022+multa-notitia@users.noreply.github.com>
2026-09-22 01:24:14 +0200

    Add Board chili body hex loader.

---
+++ src/assets/concept-a/board/chili-body.data.ts
@@ -0,0 +1,12 @@
+import { pngHexToDataUrl } from '../../pngHex.ts'
+import hex0 from './chili-body.hex.00.ts'
+import hex1 from './chili-body.hex.01.ts'
+import hex2 from './chili-body.hex.02.ts'
+import hex3 from './chili-body.hex.03.ts'
+import hex4 from './chili-body.hex.04.ts'
+import hex5 from './chili-body.hex.05.ts'
+import hex6 from './chili-body.hex.06.ts'
+import hex7 from './chili-body.hex.07.ts'
+import hex8 from './chili-body.hex.08.ts'
+
+export default pngHexToDataUrl([hex0, hex1, hex2, hex3, hex4, hex5, hex6, hex7, hex8].join(''))
